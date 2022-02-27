c1811730c84f50515956028d88dfede87d4912ad
Tran Manh Linh <devngmo@gmail.com>
2022-02-28 06:28:08 +0700

    add thumbnail cache storage, manager

---
+++ CIU-WPF/AppLayer/ThumbnailCacheManager.cs
@@ -0,0 +1,87 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIU_WPF.AppLayer
+{
+    internal class ThumbnailCacheManager
+    {
+        public class ThumbnailCacheManagerSettings
+        {
+            public string CacheFolder { get; set; }
+        }
+
+        private ThumbnailCacheManagerSettings _settings;
+
+        private static ThumbnailCacheManager s_ins = new ThumbnailCacheManager();
+        public static ThumbnailCacheManager Ins => s_ins;
+
+        public class CreateThumbnailArgs : EventArgs
+        {
+            public string originFilePath { get; set; }
+            public string thumbnailID { get; set; }
+        }
+        public EventHandler<CreateThumbnailArgs> ThumbnailCreatedEventHandler;
+        internal string createThumbnail(string filePath, int maxWidth, int maxHeight)
+        {
+            const int quality = 75;
+
+            using (var input = File.OpenRead(filePath))
+            {
+                using (var inputStream = new SKManagedStream(input))
+                {
+                    using (var original = SKBitmap.Decode(inputStream))
+                    {
+                        float dx = maxWidth * 1.0f / original.Width;
+                        float dy = maxHeight * 1.0f / original.Height;
+                        float d = dx;
+                        if (d > dy) d = dy;
+
+                        int width = (int)(original.Width * d);
+                        int height = (int)(original.Height * d);
+
+                        using (var resized = original
+                               .Resize(new SKImageInfo(width, height), SKBitmapResizeMethod.Lanczos3))
+                        {
+                            if (resized == null) return null;
+                            return ThumbnailCacheStorage.Ins.save(resized, filePath, 75);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void Init(ThumbnailCacheManagerSettings settings)
+        {
+            _settings = settings;
+            ThumbnailCacheStorage.Ins.Init(new ThumbnailCacheStorage.ThumbnailCacheStorageSettings { 
+                CacheFolder = settings.CacheFolder
+            });
+        }
+
+        internal void CreateThumbnailsFromFiles(List<string> imageFiles, int maxWidth, int maxHeight)
+        {
+            foreach (var imageFile in imageFiles)
+            {
+                string thumbnailID = ThumbnailCacheStorage.Ins.getThumbnailID(imageFile);
+                if (thumbnailID == null)
+                    thumbnailID = createThumbnail(imageFile, maxWidth, maxHeight);
+
+                if (ThumbnailCreatedEventHandler  != null)
+                    ThumbnailCreatedEventHandler.Invoke(this, new CreateThumbnailArgs { 
+                        originFilePath = imageFile,
+                        thumbnailID = thumbnailID
+                    });
+            }
+        }
+
+        internal void CreateThumbnailsFromFolder(string curFolderPath)
+        {
+
+        }
+    }
+}
---
+++ CIU-WPF/AppLayer/ThumbnailCacheStorage.cs
@@ -0,0 +1,108 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace CIU_WPF.AppLayer
+{
+    internal class ThumbnailCacheStorage
+    {
+        public class ThumbnailCacheStorageSettings
+        {
+            public string CacheFolder { get; set; }
+        }
+        private static ThumbnailCacheStorage s_ins = new ThumbnailCacheStorage();
+        public static ThumbnailCacheStorage Ins => s_ins;
+
+        ThumbnailCacheStorageSettings _settings;
+
+        Dictionary<string, string> fileMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+        public void Init(ThumbnailCacheStorageSettings settings)
+        {
+            _settings = settings;
+            if (!Directory.Exists(_settings.CacheFolder))
+                Directory.CreateDirectory(_settings.CacheFolder);   
+        }
+
+        public void Load()
+        {
+            loadFileMap();
+        }
+
+        private void loadFileMap()
+        {
+            fileMap.Clear();
+            string pathFileMap = Path.Combine(_settings.CacheFolder, "filemap.txt");
+            if (!File.Exists(pathFileMap))
+                return;
+
+            string[] lines = File.ReadAllLines(pathFileMap);
+            foreach (string line in lines)
+            {
+                try
+                {
+                    int idx = line.IndexOf(":");
+                    if (idx >= 32)
+                    {
+                        string fileID = line.Substring(0, idx);
+                        string filePath = line.Substring(idx + 1);
+                        fileMap.Add(filePath, fileID);
+                    }
+                }
+                catch { }
+            }
+        }
+
+        public string save(SKBitmap bmp, string filePath, int quality)
+        {
+            string fileID = Guid.NewGuid().ToString("N");
+            string thumbnailFilePath = Path.Combine(_settings.CacheFolder, fileID);
+            using (var image = SKImage.FromBitmap(bmp))
+            {
+                using (var output =
+                       File.OpenWrite(thumbnailFilePath))
+                {
+                    image.Encode(SKEncodedImageFormat.Png, quality)
+                        .SaveTo(output);
+
+                    fileMap.Add(filePath, fileID);
+                }
+            }
+            return fileID;
+        }
+
+        public bool hasThumbnailOf(string filePath)
+        {
+            if (fileMap.ContainsKey(filePath))
+            {
+                string fileID = fileMap[filePath];
+                string thumbnailFilePath = Path.Combine(_settings.CacheFolder, fileID);
+                return File.Exists(thumbnailFilePath);
+            }
+            return false;
+        }
+
+        internal string? getThumbnailID(string originFilePath)
+        {
+            if (fileMap.ContainsKey(originFilePath))
+                return fileMap[originFilePath];
+            return null;
+        }
+
+        public BitmapImage? loadAsBitmapImage(string filePath)
+        {
+            if (fileMap.ContainsKey(filePath))
+            {
+                string fileID = fileMap[filePath];
+                string thumbnailFilePath = Path.Combine(_settings.CacheFolder, fileID);
+                if (File.Exists(thumbnailFilePath))
+                    return new BitmapImage(new Uri(thumbnailFilePath));
+            }
+            return null;
+        }
+    }
+}
